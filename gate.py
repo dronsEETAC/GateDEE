8610685865fd340f15595aa8829e2c25c861af63
guillemaguilapalleja <guillem.aguila,palleja@estudiantat.upc.edu>
2022-12-29 18:07:50 +0100

    Changed gate

--- gate.py
+++ gate.py
@@ -45,7 +45,6 @@ def on_global_message(client, userdata, message):
         global_client.subscribe(origin + "/LEDsService/#")
         global_client.subscribe(origin + "/cameraService/#")
         global_client.subscribe(origin + "/autopilotService/#")
-        global_client.subscribe(origin + "/guillemAguila/#")
 
         # subscribe to commands from dataService
 
